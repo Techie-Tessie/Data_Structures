401ace5f803603648de2ed19c3123d673ad7a58d
Tess Watt <71218139+Tess314@users.noreply.github.com>
2021-04-21 18:14:22 +0100

    Update array.c

--- array.c
+++ array.c
@@ -7,8 +7,8 @@ int main()
 	
 	int* total = malloc(6 * sizeof(int));
 	
-	memcpy(total, t1_grades, 3 * sizeof(int)); // copy 4 ints from t1_grades to total[0]...total[2]
-	memcpy(total + 3, t2_grades, 3 * sizeof(int)); // copy 4 ints from t2_grades to total[3]...total[6]
+	memcpy(total, t1_grades, 3 * sizeof(int)); // copy 3 ints from t1_grades to total[0]...total[2]
+	memcpy(total + 3, t2_grades, 3 * sizeof(int)); // copy 3 ints from t2_grades to total[3]...total[5]
 	
 	for(int i=0; i<6; i++)
         printf("%d\n", total[i]);
